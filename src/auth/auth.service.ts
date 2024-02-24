fb6d0e9d2bd0053c4aa82975a2227b295f3ad650
tunatuna123 <tuna200538@gmail.com>
2024-02-24 22:00:56 +0900

    feat: add sign to jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
+import { JwtService } from '@nestjs/jwt';
 
 const testUsers = [
   {
@@ -16,10 +17,16 @@ const testUsers = [
 
 @Injectable()
 export class AuthService {
+  constructor(private jwtService: JwtService) {}
+
   validateUser({ username, password }: AuthPayloadDto) {
     const findUser = testUsers.find((user) => user.username === username);
     if (!findUser) {
       return null;
+    }
+    if (password === findUser.password) {
+      const { password, ...user } = findUser;
+      this.jwtService.sign(user);
     }
   }
 }
