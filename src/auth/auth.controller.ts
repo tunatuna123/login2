2c16bc7142f7cd168343d162bf049d82ec1eecad
tunatuna123 <tuna200538@gmail.com>
2024-02-24 19:54:20 +0900

    feat: check if valid user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('login')
+  logint(@Body() authPayload: AuthPayloadDto) {}
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { AuthPayloadDto } from './dto/auth.dto';
+
+const testUsers = [
+  {
+    id: 1,
+    username: 'pinky',
+    password: 'password1',
+  },
+  {
+    id: 2,
+    username: 'punky',
+    password: 'password2',
+  },
+];
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  validateUser({ username, password }: AuthPayloadDto) {
+    const findUser = testUsers.find((user) => user.username === username);
+    if (!findUser) {
+      return null;
+    }
+  }
+}
