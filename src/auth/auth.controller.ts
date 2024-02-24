0352728604d1500f5e602e7245d9da0375f82248
tunatuna123 <tuna200538@gmail.com>
2024-02-24 22:43:07 +0900

    Feat: login check

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, Post } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
   @Post('login')
   logint(@Body() authPayload: AuthPayloadDto) {
-    return this.authService.validateUser(authPayload);
+    const user = this.authService.validateUser(authPayload);
+
+    if (!user) {
+      throw new HttpException('login error', 401);
+    }
+    return user;
   }
 }
--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,7 +1,4 @@
-
-
 export class AuthPayloadDto {
-    username: string;
-    password: string;
-    
+  username: string;
+  password: string;
 }
