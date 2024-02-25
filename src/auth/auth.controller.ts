fbc1f945a40a4ba37c0683f43b77b70855f3c3e0
tunatuna123 <tuna200538@gmail.com>
2024-02-25 10:10:47 +0900

    feat: add localGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,13 +2,14 @@ import { Body, Controller, HttpException, Post, UseGuards } from '@nestjs/common
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from '@nestjs/passport';
+import { LocalGuard } from './guard/local.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  @UseGuards(AuthGuard('local'))
+  @UseGuards(LocalGuard)
   logint(@Body() authPayload: AuthPayloadDto) {
     const user = this.authService.validateUser(authPayload);
     return user;
