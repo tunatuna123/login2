75d1b4c3cb6766c5ff2f99bbf2bf4eb112599f9d
tunatuna123 <tuna200538@gmail.com>
2024-02-25 10:07:22 +0900

    feat: add guard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,18 +1,16 @@
-import { Body, Controller, HttpException, Post } from '@nestjs/common';
+import { Body, Controller, HttpException, Post, UseGuards } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { AuthService } from './auth.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
+  @UseGuards(AuthGuard('local'))
   logint(@Body() authPayload: AuthPayloadDto) {
     const user = this.authService.validateUser(authPayload);
-
-    if (!user) {
-      throw new HttpException('login error', 401);
-    }
     return user;
   }
 }
--- src/auth/strategies/local.strategy.ts
+++ src/auth/strategies/local.strategy.ts
@@ -1,11 +1,16 @@
-import { PassportStrategy } from "@nestjs/passport";
-import { Strategy } from "passport-local";
-import { AuthService } from "../auth.service";
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+import { AuthService } from '../auth.service';
+import { UnauthorizedException } from '@nestjs/common';
 
 export class LocalStrategy extends PassportStrategy(Strategy) {
-    constructor(private authService: AuthService) {
-        super();
-    }
+  constructor(private authService: AuthService) {
+    super();
+  }
 
-    validate(username: string, password: string) {}
+  validate(username: string, password: string) {
+    const user = this.authService.validateUser({ username, password });
+    if (!user) throw new UnauthorizedException();
+    return user;
+  }
 }
