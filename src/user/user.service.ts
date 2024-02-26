8c9202120f3639d83a775463c317b7f344c194f6
tunatuna123 <tuna200538@gmail.com>
2024-02-26 21:15:31 +0900

    feat: check if account with same email exists

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,13 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { User } from './user.model';
+import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: User): Promise<User> {
-    const {createHash} = require('crypto')
+    if (
+      await this.prisma.user.findUnique({
+        where: { email: String(data.email) },
+      })
+    ) {
+      throw new HttpException('Already accout with same email exist', 401);
+    }
+
+    const { createHash } = require('crypto');
     data.password = createHash('sha256').update(data.password).digest('base64');
 
     return this.prisma.user.create({
