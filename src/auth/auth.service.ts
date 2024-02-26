9afbd59f57c7bb4cdcccfced3247e9e97a3424d3
tunatuna123 <tuna200538@gmail.com>
2024-02-26 19:59:40 +0900

    chore: clean code

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,20 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { AuthPayloadDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma.service';
-import { ExceptionsHandler } from '@nestjs/core/exceptions/exceptions-handler';
-
-const testUsers = [
-  {
-    id: 1,
-    username: 'pinky',
-    password: 'password1',
-  },
-  {
-    id: 2,
-    username: 'punky',
-    password: 'password2',
-  },
-];
 
 @Injectable()
 export class AuthService {
