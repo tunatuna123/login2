496bd4d4c5dd5c0fc13a2dccfb8191aaa9d7345e
tunatuna123 <tuna200538@gmail.com>
2024-02-26 21:37:31 +0900

    Fix: typo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Put } from '@nestjs/common';
 import { User } from './user.model';
 import { UserService } from './user.service';
 
@@ -7,7 +7,8 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  async createProduct(@Body() postData: User): Promise<User> {
+  async createUser(@Body() postData: User): Promise<User> {
     return this.userService.createUser(postData);
   }
+
 }
