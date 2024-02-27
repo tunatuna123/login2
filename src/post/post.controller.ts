2091daea80dd4647fa332dd5264edd0e243c6d53
tunatuna123 <tuna200538@gmail.com>
2024-02-27 19:20:52 +0900

    feat: add post update, delete

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  UseGuards,
+  Delete,
+  Req,
+  Put,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
+import { PostService } from './post.service';
+import { PostModel } from './post.model';
+import { Request } from 'express';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private postService: PostService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getAllPost(): Promise<PostModel[]> {
+    return this.postService.getAllPost();
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getPost(@Param('id') id: number): Promise<PostModel | null> {
+    return this.postService.getPost(id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async createPost(@Body() postData: PostModel) {
+    return this.postService.createPost(postData);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete(':id')
+  async deletePost(@Param('id') id: number, @Req() req: Request) {
+    return this.postService.deletePost(id, String(req.headers.authorization));
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Put(':id')
+  async updatePost(
+    @Param('id') id: number,
+    @Body() postData: PostModel,
+    @Req() req: Request,
+  ) {
+    return this.postService.updatePost(
+      id,
+      postData,
+      String(req.headers.authorization),
+    );
+  }
+}
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { PostModel } from './post.model';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(private prisma: PrismaService) {}
+
+  async getAllPost(): Promise<PostModel[]> {
+    return this.prisma.post.findMany();
+  }
+
+  async getPost(id: number): Promise<PostModel | null> {
+    return this.prisma.post.findUnique({ where: { id: Number(id) } });
+  }
+
+  async createPost(data: PostModel): Promise<PostModel> {
+    return this.prisma.post.create({
+      data,
+    });
+  }
+
+  async deletePost(id: number, username: string) {
+    username = JSON.parse(atob(username.split('.', 3)[1])).username;
+    const targetPost = await this.prisma.post.findUnique({
+      where: { id: Number(id) },
+    });
+    if (username === JSON.parse(JSON.stringify(targetPost)).name) {
+      return this.prisma.post.delete({ where: { id: Number(id) } });
+    } else {
+      throw new HttpException("Don't have Authorization", 401);
+    }
+  }
+
+  async updatePost(id: number, data: PostModel, username: string) {
+    username = JSON.parse(atob(username.split('.', 3)[1])).username;
+    if (username === data.name) {
+      return this.prisma.post.update({
+        where: { id: Number(id) },
+        data: {
+          name: username,
+          post: data.post,
+          image: data.image,
+        },
+      });
+    } else {
+      throw new HttpException("Don't have Authorization", 401);
+    }
+  }
+}
