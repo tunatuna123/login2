b2ef4114d44c3c6d163cba6aad830a0176afee32
tunatuna123 <tuna200538@gmail.com>
2024-02-27 16:11:37 +0900

    Fix: add JwtAutGuard to user pw change

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,19 @@
-import { Body, Controller, Post, Put, Param } from '@nestjs/common';
+import { Body, Controller, Post, Put, Param, UseGuards } from '@nestjs/common';
 import { User } from './user.model';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
+  @Post('/create')
   async createUser(@Body() postData: User): Promise<User> {
     return this.userService.createUser(postData);
   }
 
-  @Put(':id')
+  @UseGuards(JwtAuthGuard)
+  @Put('/pwupdate/:id')
   async updatePassword(
     @Body() postData: JSON,
     @Param('id') id: number,
